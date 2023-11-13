78e2b0a15498dcd52dfae1369a3485af39b89057
VVVladyslav <vladoszhar@gmail.com>
2023-11-14 00:07:34 +0200

    last

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
 package org.example;
+
 import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello from Gradle!");
-
-        String jsonString = "{\"name\": \"Vladyslav\", \"lastName\": \"Zharkov\"}";
+        String name = "Vladyslav";
+        String lastName = "Zharkov";
 
         Gson g = new Gson();
-        String person = g.toJson(jsonString);
+        String person = g.toJson("{\"name\": \"" + name + "\", \"lastName\": \"" + lastName + "\"}");
 
         System.out.println(person);
-
     }
 }
